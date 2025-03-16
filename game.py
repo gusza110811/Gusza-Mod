813363d5802254fdff0c7df714201c53714256a4
gusza110811 <100755292+gusza110811@users.noreply.github.com>
2025-03-16 22:06:24 +0700

    minor changes

--- game.py
+++ game.py
@@ -3,9 +3,10 @@
 
 import pygame
 import types
+from multiprocessing import Process
 
 class vector:
-    """Base class for 2 dimensional position and scale unit"""
+    """Base class for 2 dimensional position and scale"""
 
     def __init__(self,x,y):
         self.x=x
@@ -39,8 +40,11 @@ class sprite:
         self.position = vector(x,y)
         try:
             self.sprite = pygame.image.load(spritedir)
-        except:
-            self.sprite = pygame.image.load("Defaults/MissingTexture")
+        except FileNotFoundError:
+            try:
+                self.sprite = pygame.image.load(spritedir+".png")
+            except FileNotFoundError:
+                self.sprite = pygame.image.load("Defaults/MissingTexture.png")
         
         if OnCreate:
             self.Created = types.MethodType(OnCreate,self)
@@ -54,33 +58,88 @@ class sprite:
     def Update(self):
         return
 
-class game:
-    """Function and other things related to the main game"""
-
-    active_sprites = {}
-    """Sprites currently on the level"""
-
-    def update(events):
-        for name in game.active_sprites:
-            currentsprite = game.active_sprites[name]
-
-            currentsprite.Update()
-
+class nil:
+    """A placeholder 'sprite' that replace a deleted sprite and only removed from the list when its the last sprite so other sprite's ID doesnt change"""
+    def Update():
         return
 
-    def begin():
-        def playermovement(self):
+class base:
+    """Class containing every default pre-made sprite presets"""
+
+    class player(sprite):
+        """Player Controlled Characters"""
+        def __init__(self, spritedir = "Defaults/DefaultPlayer", x = 0, y = 0):
+            super().__init__(spritedir, None, None, x, y)
+
+        def Update(self):
             keypressed = pygame.key.get_pressed()
             if keypressed[pygame.K_a]:
                 self.position.x -= 5
             if keypressed[pygame.K_d]:
                 self.position.x += 5
+            if keypressed[pygame.K_w]:
+                self.position.y -= 5
+            if keypressed[pygame.K_s]:
+                self.position.y += 5
+    
+    class follower(sprite):
+        """Basic Character that can be used for enemy characters but there is no health system in the base game"""
+        def __init__(self, spritedir = "Defaults/MissingTexture", OnCreate=None, x = 0, y = 0):
+            super().__init__(spritedir, OnCreate, x, y)
 
-            return
-        game.active_sprites["player"] = sprite(spritedir="Defaults/DefaultPlayer.png",x=20,y=20, OnUpdate=playermovement)
+        def Update(self):
+
+            try:
+                player = next(filter(lambda a: type(a) is base.player,game.active_sprites))
+            except StopIteration:
+                return
+            
+            if self.position.x > player.position.x:
+                self.position.x -= 2
+            else:
+                self.position.x += 2
+            
+            if self.position.y > player.position.y:
+                self.position.y -= 2
+            else:
+                self.position.y += 2
+
+class game:
+    """Function and other things related to the main game"""
+
+    active_sprites:list[sprite] = []
+    """Sprites currently on the level"""
+
+    def kill(spriteID):
+        game.active_sprites[spriteID] = nil
+
+    def update(events):
+
+        for item in game.active_sprites:
+            currentsprite = item
+
+            currentsprite.Update()
+        
+        while game.active_sprites[-1] is nil:
+            game.active_sprites.pop()
+
+        return
+
+    def begin():
+        game.active_sprites.append(base.player())
+        game.active_sprites.append(base.follower())
+        game.active_sprites.append(base.follower())
+        game.kill(2)
 
         return
     
+
+class commandterminal:
+    def main():
+        print(">------------------------<")
+        print(">----Command terminal----<")
+        print(">Enter command to execute<")
+        command = input(">")
 
 class engine:
     """Function and other things not related to the main game"""
@@ -96,7 +155,7 @@ class engine:
         game.begin()
 
         while engine.running:
-            engine.screen.fill(pygame.Color(100,100,128))
+            engine.screen.fill(pygame.Color(64,128,200))
             events = pygame.event.get()
 
             for event in events:
@@ -113,9 +172,9 @@ class engine:
         return
     
     def render():
-        for name in game.active_sprites:
-            rendersprite:sprite=game.active_sprites[name]
-            
+        for rendersprite in game.active_sprites:
+            if rendersprite is nil: continue
+
             position = rendersprite.position-viewport.position+(viewport.size/vector(2,2))
 
             engine.screen.blit(rendersprite.sprite,position)
